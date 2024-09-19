fac0442d3fde1a80064fded3d4777f030fe89697
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2024-09-19 20:26:36 +0700

    commit to save current progress

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				"bookshelf-primary": "#00B0FF",
+				"bookshelf-dark": "#0C0C0C",
+				"bookshelf-light": "#F0F0F0",
+			},
+		},
+	},
+	plugins: [],
+};
